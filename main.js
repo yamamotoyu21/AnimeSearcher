88aa6c26e1390a2577e1bc21facb19622c287193
yamamotoyu21 <yu.yamamoto@rakus-partners.co.jp>
2023-01-09 20:30:42 +0900

    added dependancies

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>アニメ検索</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
